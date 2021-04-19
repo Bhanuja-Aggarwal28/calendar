c84dd4d5023e81f30a4f19f1fc2839238a74cbf6
bhanuja-aggarwal28 <bhanuja.aggarwal@cygrp.com>
2021-04-19 19:57:25 +0530

    first commit

---
+++ month/style.js
@@ -0,0 +1,79 @@
+function jan(){
+  window.open("jan.html","_self");
+}
+function feb(){
+  window.open("feb.html","_self");
+}
+function mar(){
+  window.open("mar.html","_self");
+}
+function apr(){
+  window.open("apr.html","_self");
+}
+function may(){
+  window.open("may.html","_self");
+}
+function june(){
+  window.open("june.html","_self");
+}
+function july(){
+  window.open("july.html","_self");
+}
+function aug(){
+  window.open("aug.html","_self");
+}
+function sep(){
+  window.open("sep.html","_self");
+}
+function oct(){
+  window.open("oct.html","_self");
+}
+function nov(){
+  window.open("nov.html","_self");
+}
+function dec(){
+  window.open("dec.html","_self");
+}
+function new_year(){
+  alert("Its new year eve.May the New Year bless you with health, wealth, and happiness.")
+}
+function republic(){
+  alert("Happy republic day . Our nation is the greatest country in the world, but let that never stop us from striving to make it even better")
+}
+function holi(){
+  alert("Happy holi. Hope you have a colorful day and a colorful life with all kind of right twists in the color.")
+}
+function good_fri(){
+alert("Its a good friday.");
+}
+function ramzan(){
+  alert("Its Ramzan/Eid-ul-Fitr. May Allah protect you from all sins and bring you peace, joy, and hope");
+}
+function memorial(){
+  alert("Its meorial day . Together we pay homage to those who have given us our freedom.");
+}
+function ind(){
+  alert("Happy independence day.Vande Matram ");
+}
+function janam(){
+  alert("Happy janamashtami. May lord Krishna shower their blessings to you and your family")
+}
+function labor(){
+
+  alert("happy labor day . we salute all those who work hard every day for us");
+}
+function duss(){
+  alert("happy dusherra to all. MAy the veil inside us die and we all live wuth peace");
+}
+function diwali(){
+  alert("May the festival of lights bring light to ur life . Happy diwali");
+}
+function thanks(){
+  alert("Bring happiness on others by gifting something good on this thanks giving");
+}
+function christams(){
+  alert("Jingke bell jingke bell jingle all the way . Merry christmas");
+}
+function ny_eve(){
+  alert("Its the last day of the year");
+}
